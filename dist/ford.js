49ac400b342459662c712dc5eae0932e0e7d8c1c
Jason Miller <jason@developit.ca>
2015-01-19 13:21:11 -0500

    Dist for 0.5.0

---
+++ dist/ford.js
@@ -0,0 +1,129 @@
+(function(g, f) {
+	if (typeof define==='function' && define.amd)
+		define([], f);
+	else if (typeof module==='object' && module.exports)
+		module.exports = f();
+	else
+		g.$ = f();
+}(this, function() {
+	var s = document.createElement('div');
+
+	function $(p,w) {
+		return $.extend(
+			typeof p==='string' ? (
+				p[0]==='<' ? (s.innerHTML=p, p=s.childNodes) : (w || document).querySelectorAll(p)
+			) : $.arr(p),
+			$.fn
+		);
+	}
+
+	$.fn = {};
+
+	$.arr = function(obj) {
+		return Array.isArray(obj) ? obj : Array.prototype[typeof obj.length==='number'?'slice':'concat'].call(obj);
+	};
+
+	$.extend = function(o, p, i) {
+		for (i in p)
+			if (p.hasOwnProperty(i))
+				o[i] = p[i];
+		return o;
+	};
+
+	$.iterate = function(c, f) {
+		for (var i=0, a, len=c.length; i<len; i++) {
+			a = f.call(c, c[i], i);
+			if (a!==undefined)
+				return a;
+		}
+		return c;
+	};
+
+	function wrap(method, ns, ret) {
+		return function() {
+			var a = arguments, i;
+			return $.iterate(this, function(c, o, j, p) {
+				o = ns && c[ns] || c;
+				j = o[method];
+				p = (j || method).apply(o, a);
+				return ret===false ? i : p;
+			});
+		};
+	}
+
+	function map(method, ns) {
+		return function() {
+			for (var i=0,a=[],c,p,g=$.arr(arguments); i<this.length; i++) {
+				c = ns || this[i];
+				p = c[method] || method || g.shift();
+				a.push.apply(a, $.arr( p.apply ? p.apply(c, g) : p ));
+			}
+			return $(a);
+		};
+	}
+
+
+	$.extend($.fn, {
+		map : map,
+		_wrap : wrap,
+		each : wrap(function(f){ return f(this); }),
+
+		children : map('children'),
+		append : wrap('appendChild', 0, false),
+		appendTo : function(p){ return $(p).append(this); },
+		remove : wrap('remove', 0, false),
+		parent : function(s,p){ p=this[0]; while((p=p.parentNode) && (s && !p.matches(s))); return p; },
+
+		on : wrap('addEventListener', 0, false),
+		off : wrap('removeEventListener', 0, false),
+
+		classify : wrap('add', 'classList', false),
+		declassify : wrap('remove', 'classList', false),
+		hasClass : wrap('contains', 'classList'),
+		toggleClass : wrap('toggle', 'classList', false),
+
+		css : wrap(function(s, c, p) {
+			if (!s || s.charAt) return p=this.style, (c = this.offsetParent ? getComputedStyle(this,null) : p) && s ? (c[s] || p[s]) : c;
+			$.extend(this.style, s);
+		}),
+
+		text : wrap(function(t) {
+			if (t==null) return this.textContent;
+			this.textContent = t;
+		}),
+		html : wrap(function(h) {
+			if (h==null) return this.innerHTML;
+			this.innerHTML = h;
+		}),
+
+		attr : wrap(function(k, v) {
+			return this[(v==null?'g':'s') + 'etAttribute'](k, v);
+		}),
+		prop : wrap(function(k, v) {
+			if (v==null) return v=this[k], v==null?null:v;
+			this[k] = v;
+		}),
+
+		tpl : wrap(function(fields) {
+			$('[tpl]', this).each(function(n) {
+				var tpl = n.getAttribute('tpl').replace(/\s+/,''),
+					r = /(.+?)\:(.+?)(;|$)/g,
+					t, k, i, v;
+				r.lastIndex = 0;
+				while ((t=r.exec(tpl))) {
+					k = $.tplMap[t[1]] || t[1];
+					v = fields[t[2]];
+					(typeof n[k]==='function' ? n[k](v) : (n[k] = v));
+				}
+			});
+		})
+	});
+
+	$.tplMap = {
+		html : 'innerHTML',
+		text : 'textContent',
+		attr : 'setAttribute'
+	};
+
+	return $;
+}));

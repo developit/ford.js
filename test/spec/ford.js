5edb41591a8b0bc19c0759e2b484aa945bf22678
Jason Miller <jason@developit.ca>
2015-03-01 13:17:12 -0500

    Tests

--- test/spec/ford.js
+++ test/spec/ford.js
@@ -40,20 +40,20 @@ describe('$.extend()', function() {
 	it('should be a function', function() {
 		expect($.extend).to.be.a('function');
 	});
-	
+
 	it('should copy own-properties and not prototype properties', function() {
 		function Test() {
 			this.prop = 'prop';
 			this.prop2 = 'prop2';
 		}
 		Test.prototype.protoProp = 'protoProp';
-		
+
 		var orig = {
 			foo : 'bar'
 		};
-		
+
 		var ret = $.extend(orig, new Test());
-		
+
 		expect( ret ).to.equal( orig );
 		expect( ret ).to.be.an.instanceof( Object );
 		expect( ret ).to.have.keys('foo', 'prop', 'prop2');
@@ -74,7 +74,7 @@ describe('$.iterate()', function() {
 	it('should be a function', function() {
 		expect($.iterate).to.be.a('function');
 	});
-	
+
 	it('should call fn on each item in an Array', function() {
 		var iterable = sinon.spy();
 		$.iterate(['a','b','c'], iterable);
@@ -82,7 +82,7 @@ describe('$.iterate()', function() {
 		expect( iterable.secondCall ).to.have.been.calledWith('b', 1);
 		expect( iterable.thirdCall ).to.have.been.calledWith('c', 2);
 	});
-	
+
 	it('should break out of the loop when a value is returned', function() {
 		var iterable = sinon.spy(function() {
 			return 'test';
@@ -101,19 +101,19 @@ describe('$(..).each()', function() {
 			document.createElement('div')
 		],
 		sel = $(nodes);
-	
+
 	it('should be a function', function() {
 		expect( sel.each ).to.be.a('function');
 	});
-	
+
 	it('should be called on each node in the selection', function() {
 		//var spy = sinon.spy(sel, 'each');
 		var spy = sinon.spy(),
 			ret = sel.each(spy);
-		
+
 		expect( spy ).to.have.been.calledTwice;
-		expect( spy.firstCall ).to.have.been.calledWithExactly(nodes[0]);
-		expect( spy.secondCall ).to.have.been.calledWithExactly(nodes[1]);
+		expect( spy.firstCall.args[0][0] ).to.equal(nodes[0]);
+		expect( spy.secondCall.args[0][0] ).to.equal(nodes[1]);
 		expect( ret ).to.equal( sel );
 	});
 });
@@ -125,29 +125,29 @@ describe('$(..).children()', function() {
 			document.createElement('div')
 		],
 		sel = $(nodes);
-	
+
 	nodes[0].innerHTML = '\n\t<div> <span>te st</span> </div>\n\n <p>test 2</p>';
 	nodes[1].innerHTML = '<div><span>test</span></div><p>test 2</p>';
-	
+
 	it('should be a function', function() {
 		expect( sel.children ).to.be.a('function');
 	});
-	
+
 	it('should return the correct children', function() {
 		expect( sel.children() ).to.have.length(4);
-		
+
 		expect( sel.children()[0] ).to.have.property('nodeName', 'DIV');
 		expect( sel.children()[0] ).to.have.property('innerHTML', ' <span>te st</span> ');
-		
+
 		expect( sel.children()[1] ).to.have.property('nodeName', 'P');
-		
+
 		expect( sel.children()[2] ).to.have.property('nodeName', 'DIV');
 		expect( sel.children()[2] ).to.have.property('innerHTML', '<span>test</span>');
-		
+
 		expect( sel.children()[3] ).to.have.property('nodeName', 'P');
-		
+
 	});
-	
+
 	it('should only return element nodes (not #comment, #text, etc)', function() {
 		var node = document.createElement('div');
 		node.innerHTML = '-  oin2g4 <!-- test comment --> \n \t <!DOCTYPE html> jt';
@@ -158,7 +158,7 @@ describe('$(..).children()', function() {
 
 describe('$(..).on()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should call addEventListener when invoked', function() {
 		var spy = sinon.stub();
 		node.addEventListener = spy;
@@ -172,7 +172,7 @@ describe('$(..).on()', function() {
 
 describe('$(..).off()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should call removeEventListener when invoked', function() {
 		var spy = sinon.stub();
 		node.removeEventListener = spy;
@@ -187,12 +187,12 @@ describe('$(..).off()', function() {
 describe('$(..).classify()', function() {
 	var node = document.createElement('div');
 	node.className = 'already-added';
-	
+
 	it('should add a class if it does not exist', function() {
 		$(node).classify('test');
 		expect( node.className ).to.equal( 'already-added test' );
 	});
-	
+
 	it('should not modify className if a class already exists', function() {
 		var className = node.className;
 		$(node).classify('already-added');
@@ -204,12 +204,12 @@ describe('$(..).classify()', function() {
 describe('$(..).declassify()', function() {
 	var node = document.createElement('div');
 	node.className = 'exists';
-	
+
 	it('should remove a class if it exists', function() {
 		$(node).declassify('exists');
 		expect( node.className ).to.be.empty;
 	});
-	
+
 	it('should not modify className if a class doesn\'t exist', function() {
 		var className = node.className;
 		$(node).declassify('does-not-exist');
@@ -221,12 +221,12 @@ describe('$(..).declassify()', function() {
 describe('$(..).hasClass()', function() {
 	var node = document.createElement('div');
 	node.className = 'exists also-exists';
-	
+
 	it('should return true if a class exists', function() {
 		expect( $(node).hasClass('exists') ).to.equal( true );
 		expect( $(node).hasClass('also-exists') ).to.equal( true );
 	});
-	
+
 	it('should return false if a class doesn\'t exist', function() {
 		expect( $(node).hasClass('does-not-exist') ).to.equal( false );
 		// try a removed class
@@ -238,20 +238,20 @@ describe('$(..).hasClass()', function() {
 
 describe('$(..).toggleClass()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should add a class if it doesn\'t exist', function() {
 		node.className = '';
 		$(node).toggleClass('new-class');
 		expect( node.className ).to.equal( 'new-class' );
 		node.className = '';
 	});
-	
+
 	it('should remove a class if it exists', function() {
 		node.className = 'existing-class';
 		$(node).toggleClass('existing-class');
 		expect( node.className ).to.equal( '' );
 	});
-	
+
 	it('should not leave residue', function() {
 		node.className = '';
 		$(node).toggleClass('foo').toggleClass('foo');
@@ -262,7 +262,7 @@ describe('$(..).toggleClass()', function() {
 
 describe('$(..).css()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should apply styling when an object is passed', function() {
 		node.style.cssText = '';
 		$(node).css({
@@ -271,25 +271,25 @@ describe('$(..).css()', function() {
 		});
 		expect( node.style.cssText.trim() ).to.equal( 'top: 5px; left: -5px;' );
 	});
-	
+
 	it('should return a detached node\'s current style when passed nothing', function() {
 		node.style.cssText = 'border:1px solid #000; padding:5px;';
 		expect( $(node).css().border ).to.match(/^1px\s+solid\s+(#000(000)?|rgba?\(0\,\s*0\,\s*0\))$/g);	// yuck
 		expect( $(node).css() ).to.have.property('padding', '5px');
 	});
-	
+
 	it('should return a detached node\'s current style property when passed a string', function() {
 		node.style.cssText = 'border:1px solid #000; padding:2px 1px;';
 		expect( $(node).css('padding') ).to.equal( '2px 1px' );
 	});
-	
+
 	it('should return computed style when passed nothing', function() {
 		document.body.appendChild(node);
 		node.style.cssText = 'border:none;';
 		expect( $(node).css() ).to.deep.equal( getComputedStyle(node) );
 		expect( $(node).css().border ).to.match(/(\b0(px)?\b|\bnone\b|^$)/g);
 	});
-	
+
 	it('should return computed style property when passed a string', function() {
 		document.body.appendChild(node);
 		node.style.cssText = 'border:1px solid #000; padding:1px;';
@@ -301,24 +301,24 @@ describe('$(..).css()', function() {
 
 describe('$(..).text()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should return textual content when passed nothing', function() {
 		node.textContent = 'test';
 		expect( $(node).text() ).to.equal('test');
 	});
-	
+
 	it('should return textual content of node and all descendants', function() {
 		node.innerHTML = 'foo <p>test inner <span>content</span></p> bar';
 		expect( $(node).text() ).to.equal( 'foo test inner content bar' );
 	});
-	
+
 	it('should set node text when passed a string', function() {
 		node.innerHTML = '<p>foo</p> bar';
 		$(node).text('test text');
 		expect( node.textContent ).to.equal('test text');
 		expect( node.innerHTML ).to.equal('test text');
 	});
-	
+
 	it('should not parse HTML', function() {
 		var str = 'foo <p>test inner content</p> bar';
 		$(node).text(str);
@@ -326,7 +326,7 @@ describe('$(..).text()', function() {
 		expect( node.childNodes ).to.be.have.length(1);
 		expect( node.childNodes[0] ).to.have.property('nodeType', 3);
 	});
-	
+
 	it('should clear node contents when passed an empty string', function() {
 		node.innerHTML = 'blah';
 		$(node).text('');
@@ -338,15 +338,15 @@ describe('$(..).text()', function() {
 
 describe('$(..).html()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should return HTML content when passed nothing', function() {
 		node.textContent = 'test';
 		expect( $(node).html() ).to.equal('test');
-		
+
 		node.innerHTML = '<div>test <b>data</b></div>';
 		expect( $(node).html() ).to.equal( '<div>test <b>data</b></div>' );
 	});
-	
+
 	it('should set HTML content when passed a string', function() {
 		var str = 'foo <p>test <b>inner</b> content</p><!-- bar -->';
 		$(node).html(str);
@@ -358,7 +358,7 @@ describe('$(..).html()', function() {
 		expect( node.childNodes[1] ).to.have.property('nodeName', 'P');
 		expect( node.childNodes[2] ).to.have.property('nodeType', 8);		// #comment node
 	});
-	
+
 	it('should clear node contents when passed an empty string', function() {
 		node.innerHTML = 'foo <p>test <b>inner</b> content</p><!-- bar -->';
 		$(node).html('');
@@ -370,24 +370,24 @@ describe('$(..).html()', function() {
 
 describe('$(..).attr()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should return attribute value when passed an existent name', function() {
 		node.setAttribute('test', 'value');
 		expect( $(node).attr('test') ).to.equal('value');
 	});
-	
+
 	it('should return attribute value when passed a non-existent name', function() {
 		expect( $(node).attr('does-not-exist') ).to.equal(null);
 	});
-	
+
 	it('should set attribute value when passed a name and value', function() {
 		$(node).attr('new-attribute', 'new-value');
 		expect( node.getAttribute('new-attribute') ).to.equal( 'new-value' );
-		
+
 		$(node).attr('new-attribute', 'updated-value');
 		expect( node.getAttribute('new-attribute') ).to.equal( 'updated-value' );
 	});
-	
+
 	it('should empty attribute value when passed a name and empty string', function() {
 		$(node).attr('thing', '');
 		expect( node.getAttribute('thing') ).to.be.empty;
@@ -397,24 +397,24 @@ describe('$(..).attr()', function() {
 
 describe('$(..).prop()', function() {
 	var node = document.createElement('div');
-	
+
 	it('should return attribute value when passed an existent name', function() {
 		node.test = 'value';
 		expect( $(node).prop('test') ).to.equal('value');
 	});
-	
+
 	it('should return attribute value when passed a non-existent name', function() {
 		expect( $(node).prop('doesNotExist') ).to.equal( null );
 	});
-	
+
 	it('should set attribute value when passed a name and value', function() {
 		$(node).prop('newProp', 5);
 		expect( node.newProp ).to.equal( 5 );
-		
+
 		$(node).prop('newProp', 'changedValue');
 		expect( node.newProp ).to.equal( 'changedValue' );
 	});
-	
+
 	it('should empty attribute value when passed a name and empty string', function() {
 		$(node).prop('thing', '');
 		expect( node.thing ).to.be.empty;
@@ -427,24 +427,24 @@ describe('$(..).remove()', function() {
 			document.createElement('div'),
 			document.createElement('div')
 		];
-	
+
 	it('should do nothing if the node has no parent', function() {
 		$(nodes).remove();
 		expect( nodes[0].parentNode ).to.equal(null);
 		expect( nodes[0].offsetParent ).to.equal(null);
 	});
-	
+
 	it('should remove a node if it has a parent', function() {
 		document.body.appendChild(nodes[0]);
 		document.body.appendChild(nodes[1]);
-		
+
 		expect( nodes[0].parentNode ).to.equal(document.body);
 		expect( nodes[0].offsetParent ).to.equal(document.body);
 		expect( nodes[1].parentNode ).to.equal(document.body);
 		expect( nodes[1].offsetParent ).to.equal(document.body);
-		
+
 		$(nodes).remove();
-		
+
 		expect( nodes[0].parentNode ).to.equal(null);
 		expect( nodes[0].offsetParent ).to.equal(null);
 		expect( nodes[1].parentNode ).to.equal(null);
